8ba9783da74805480b986687f16606f200312a92
AgnosticSix <mateo_godfather@hotmail.com>
2025-02-22 19:53:42 -0600

    Agregar estructura inicial del proyecto ms-eureka con configuración de Maven y pruebas unitarias

---
+++ Backend/ms-eureka/src/main/java/mx/equipo_g_1004/unir/mseureka/MsEurekaApplication.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaApplication.class, args);
+    }
+
+}
