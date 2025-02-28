cbaf8e7749fe8a57ee52292739bddbf3fd0ad126
AgnosticSix <mateo_godfather@hotmail.com>
2025-02-28 14:32:54 -0600

    Agregar configuración inicial para los microservicios ms-cloud-gateway y ms-eureka

---
+++ Backend/ms-cloud-gateway/src/main/java/mx/equipo_g_1004/unir/ms_cloud_gateway/MsCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.ms_cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCloudGatewayApplication.class, args);
+	}
+
+}
