8d50131512dddc9d3917f0f3a27a0108d18a0472
Cristofer <crismaverdeveloper@gmail.com>
2025-03-03 23:05:57 -0600

    ms-search

---
+++ Backend/ms-search/src/main/java/com/mexflix/search/MsSearchApplication.java
@@ -0,0 +1,11 @@
+package com.mexflix.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsSearchApplication.class, args);
+    }
+}
---
+++ Backend/ms-search/src/test/java/com/mexflix/search/AppTest.java
@@ -0,0 +1,11 @@
+package com.mexflix.search;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testExample() {
+        assertTrue(true, "Elastic test está funcionando correctamente.");
+    }
+}
