36bc66d7a0db1f42ebd08a19a33e9104336cd74a
cruvalcabam <claudia.ruvalcabam@gmail.com>
2025-03-03 23:14:54 -0600

    Add files via upload

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -3,6 +3,7 @@ import { getMovieInfo } from "../utils/apiMovies";
 import { MovieCard } from "./MovieCard";
 import styles from "./MoviesGrid.module.css";
 import { useQuery } from "../utils/useQuery";
+import { Paginator } from 'primereact/paginator';
 
 export function MoviesGrid() {
   const [movies, setMovies] = useState([]);
@@ -19,11 +20,24 @@ export function MoviesGrid() {
     });
   }, [search]);
 
+  const [first, setFirst] = useState(0);
+
+  const onPageChange = (event) => {
+      setFirst(event.first);
+  };
+
   return (
-    <ul className={styles.moviesGrid}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </ul>
+    <div>       
+        <div className={styles.card}>
+            {movies.slice(first, first + 1).map((movie) => (<MovieCard key={movie.id} movie={movie} />))}
+        </div>   
+        <Paginator 
+          className={styles.pages}
+          first={first} 
+          rows={1} 
+          totalRecords={movies.length} 
+          onPageChange={onPageChange} 
+          template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink" />  
+    </div>
   );
 }
